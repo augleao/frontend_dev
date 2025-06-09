435ac60c8e28e94de4389013f6fe17fcbf3f3479
augleao <augleao@gmail.com>
2025-06-09 16:06:36 -0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 // src/config.js
 const config = {
-  apiURL: process.env.REACT_APP_API_URL || 'http://localhost:3001/api'
+  apiURL: process.env.REACT_APP_API_URL || 'https://backend-dev-ypsu.onrender.com/api'
 };
 
 export default config;
