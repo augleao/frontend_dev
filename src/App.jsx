9a3e5ec2ec21dee505557625d04d56b00e26f9dc
augleao <106026886+augleao@users.noreply.github.com>
2025-06-09 11:36:35 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Home';
-import Conciliacao from './components/Conciliacao';
+import Conciliacao from './Conciliacao';
 
 function App() {
   return (
