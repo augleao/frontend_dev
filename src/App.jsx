0120d3d44b4c0cc12d48cd872363da76e8b9c667
augleao <106026886+augleao@users.noreply.github.com>
2025-06-09 11:30:11 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/conciliacao" element={<Conciliacao />} />
+        <Route path="./components/conciliacao" element={<Conciliacao />} />
       </Routes>
     </Router>
   );
