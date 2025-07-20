cd379f77146f826f6c03aba0de23d0a82382feff
augleao <augleao@gmail.com>
2025-07-20 14:31:10 -0300

    Update ServicoLista.jsx

--- src/components/servicos/ServicoLista.jsx
+++ src/components/servicos/ServicoLista.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react";
+import React, { useEffect, useState } from 'react';
 
 export default function ServicoLista({ servicos: initialServicos, filtro, setFiltro, tiposServico, statusExecucao, statusPagamento, onVerDetalhes }) {
   const [servicos, setServicos] = useState(initialServicos);
