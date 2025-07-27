adad436684707a3ea6f9db746ed69c0d047efcc2
augleao <augleao@gmail.com>
2025-07-27 12:20:54 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
-
-console.log('Iniciando index.js');
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom'; // <--- adicione isso
+import { BrowserRouter } from 'react-router-dom';
 
+console.log('Iniciando index.js');
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 console.log('Renderizando App com BrowserRouter');
