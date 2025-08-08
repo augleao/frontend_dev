b2ce733fceedb76c435dac564ef9c73bedb05663
augleao <augleao@gmail.com>
2025-08-07 23:55:42 -0300

    Update backupAgendadoService.js

--- src/backupAgendadoService.js
+++ src/backupAgendadoService.js
@@ -4,7 +4,7 @@ import config from './config';
 
 export async function getBackupAgendado(postgresId) {
   const token = localStorage.getItem('token');
-  const res = await fetch(`${config.apiURL}/admin/backupAgendado/${postgresId}`, {
+  const res = await fetch(`${config.apiURL}/${postgresId}/backup-agendado`, {
     headers: {
       'Authorization': `Bearer ${token}`,
       'Accept': 'application/json'
@@ -16,7 +16,7 @@ export async function getBackupAgendado(postgresId) {
 
 export async function saveBackupAgendado(postgresId, horario, ativo) {
   const token = localStorage.getItem('token');
-  const res = await fetch(`${config.apiURL}/admin/backupAgendado/${postgresId}`, {
+  const res = await fetch(`${config.apiURL}/${postgresId}/backup-agendado`, {
     method: 'POST',
     headers: {
       'Authorization': `Bearer ${token}`,
