bf75c6108abcb59eefe2138915ddfe7d03259743
augleao <augleao@gmail.com>
2025-08-10 16:51:20 -0300

    Update ServicoManutencao.jsx

--- src/components/servicos/ServicoManutencao.jsx
+++ src/components/servicos/ServicoManutencao.jsx
@@ -1,4 +1,3 @@
-import React, { useState, useEffect, useCallback } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import ServicoEntrada from './ServicoEntrada';
 import ServicoCliente from './ServicoCliente';
