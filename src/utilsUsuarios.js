50a8e1c983c2ff97752131313bc9285b5f2475f4
augleao <augleao@gmail.com>
2025-08-22 08:12:12 -0300

    Update utilsUsuarios.js

--- src/utilsUsuarios.js
+++ src/utilsUsuarios.js
@@ -1,7 +1,8 @@
+import { apiURL } from './config';
 // Retorna uma Promise que resolve para um Map de idUsuario -> usuario (com campo serventia)
 export async function getUsuariosMap() {
   const token = localStorage.getItem('token');
-  const res = await fetch(`${require('../../config').default.apiURL}/admin/usuarios`, {
+  const res = await fetch(`${apiURL}/admin/usuarios`, {
     headers: { Authorization: `Bearer ${token}` },
   });
   const data = await res.json();
