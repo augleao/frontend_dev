752a367f9fcfb726c8de19951e09d252839d70ff
augleao <augleao@gmail.com>
2025-08-22 10:06:13 -0300

    Update ServicoEntrada.jsx

--- src/components/servicos/ServicoEntrada.jsx
+++ src/components/servicos/ServicoEntrada.jsx
@@ -1144,10 +1144,10 @@ export default function ServicoEntrada({ form, tiposServico, onChange, combosDis
                     <th style={{ padding: 6, color: '#6c3483', fontWeight: 700, fontSize: 10, textAlign: 'center', verticalAlign: 'middle' }}>Combo</th>
                     <th style={{ padding: 6, width: 70, minWidth: 50, maxWidth: 80, color: '#6c3483', fontWeight: 700, fontSize: 10, textAlign: 'center', verticalAlign: 'middle' }}>Código do Ato</th>
                     <th style={{ padding: 6, color: '#6c3483', fontWeight: 700, fontSize: 10, textAlign: 'center', verticalAlign: 'middle' }}>Descrição do Ato</th>
-                    <th style={{ padding: 6, width: 50, minWidth: 50, maxWidth: 50, color: '#6c3483', fontWeight: 700, fontSize: 10, textAlign: 'center', verticalAlign: 'middle' }}>Quantidade</th>
+                    <th style={{ padding: 6, width: 40, minWidth: 40, maxWidth: 40, color: '#6c3483', fontWeight: 700, fontSize: 10, textAlign: 'center', verticalAlign: 'middle' }}>Quantidade</th>
                     <th style={{ padding: 6, width: 60, minWidth: 60, maxWidth: 60, color: '#6c3483', fontWeight: 700, fontSize: 10, textAlign: 'center', verticalAlign: 'middle' }}>Código Tributário</th>
-                    <th style={{ padding: 6, color: '#6c3483', fontWeight: 700, fontSize: 10, textAlign: 'center', verticalAlign: 'middle' }}>Tipo de Registro</th>
-                    <th style={{ padding: 6, color: '#6c3483', fontWeight: 700, fontSize: 10, textAlign: 'center', verticalAlign: 'middle' }}>Nome do(s) Registrado(s)</th>
+                    <th style={{ padding: 6, width: 100, minWidth: 100, maxWidth: 100, color: '#6c3483', fontWeight: 700, fontSize: 10, textAlign: 'center', verticalAlign: 'middle' }}>Tipo de Registro</th>
+                    <th style={{ padding: 6, width: 170, minWidth: 150, maxWidth: 180, color: '#6c3483', fontWeight: 700, fontSize: 10, textAlign: 'center', verticalAlign: 'middle' }}>Nome do(s) Registrado(s)</th>
                     <th style={{ padding: 6, color: '#6c3483', fontWeight: 700, fontSize: 10, textAlign: 'center', verticalAlign: 'middle' }}>Livro</th>
                     <th style={{ padding: 6, color: '#6c3483', fontWeight: 700, fontSize: 10, textAlign: 'center', verticalAlign: 'middle' }}>Folha</th>
                     <th style={{ padding: 6, color: '#6c3483', fontWeight: 700, fontSize: 10, textAlign: 'center', verticalAlign: 'middle' }}>Termo</th>
