ca6467cc073318d9a830559ab56b69602060e4ed
augleao <augleao@gmail.com>
2025-08-28 19:08:58 -0300

    Update ServicoPagamento.jsx

--- src/components/servicos/ServicoPagamento.jsx
+++ src/components/servicos/ServicoPagamento.jsx
@@ -567,7 +567,7 @@ export default function ServicoPagamento({ form, onChange, valorTotal = 0, valor
             }}
             style={{
               width: 120,
-              padding: '8px 12px',
+              padding: '3px 6px', // padding reduzido para aproximar placeholder das bordas
               borderRadius: 6,
               border: '1px solid #e0b9b9ff',
               fontSize: '16px',
