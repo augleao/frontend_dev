a9e6543c053b04bd682020c5c9b92cf70e6bbba2
augleao <augleao@gmail.com>
2025-08-30 11:22:05 -0300

    Update Home2.jsx

--- src/Home2.jsx
+++ src/Home2.jsx
@@ -158,7 +158,7 @@ function Home() {
           fontWeight: '600',
           letterSpacing: '0.5px'
         }}>
-          Sistema Auxiliar do RCPN v1.4.2
+          Sistema Auxiliar do RCPN v1.4.2.1
         </h1>
       </header>
 
