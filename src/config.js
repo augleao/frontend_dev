f65be0ba4a7d2598a48b4c6571639848802bdff2
augleao <106026886+augleao@users.noreply.github.com>
2025-09-18 14:25:47 -0300

    ok

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 // src/config.js
 const config = {
-  apiURL: process.env.REACT_APP_API_URL || 'https://backend-dev-ypsu.onrender.com/api',
-//  apiURL: process.env.REACT_APP_API_URL || 'https://backend-goby.onrender.com/api',
+ // apiURL: process.env.REACT_APP_API_URL || 'https://backend-dev-ypsu.onrender.com/api',
+  apiURL: process.env.REACT_APP_API_URL || 'https://backend-goby.onrender.com/api',
 };
 
 // Export nomeado
