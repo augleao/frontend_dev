ec9626f4540b8c5881f293fb78ca1b8451123730
augleao <augleao@gmail.com>
2026-01-04 13:49:40 -0300

    ok

--- migrations/20251125_add_codigo_tributario_to_selos.sql
+++
@@ -1,8 +0,0 @@
--- Migration: add codigo_tributario to selos_execucao_servico
--- Run this migration against the database used by the backend API that handles selos
-
-ALTER TABLE selos_execucao_servico
-  ADD COLUMN codigo_tributario varchar(128);
-
--- Optionally, if you want an index for faster lookup by code:
--- CREATE INDEX idx_selos_codigo_tributario ON selos_execucao_servico (codigo_tributario);
